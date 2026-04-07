8be960a3a6ab1b57b681a110a55a4f2e5b983d13
Tapan Radadiya <radadiyatapan90@gmail.com>
2026-04-07 18:06:22 +0000

    add monitoring libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { WinstonModule } from 'nest-winston';
+import LokiTransport from 'winston-loki';
+import { PrometheusModule } from "@willsoto/nestjs-prometheus"
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new LokiTransport({
+          host: 'http://192.168.1.31:3100',
+          labels: {
+            app: 'NestJs Observability Stack'
+          },
+          json: true
+        }),
+      ]
+    }),
+    PrometheusModule.register({
+      path: '/metrics/logger'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
